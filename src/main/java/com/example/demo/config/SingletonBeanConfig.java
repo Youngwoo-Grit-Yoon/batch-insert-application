6fb32dec42448e6238128b995a5cc8ca1f2459a3
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-08 18:08:33 +0900

    Add SingletonBeanConfig class

--- src/main/java/com/example/demo/config/SingletonBeanConfig.java
+++ src/main/java/com/example/demo/config/SingletonBeanConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.data.JsonData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +9,12 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class SingletonBeanConfig {
+    @Value("${json-data-file-path}")
+    private String jsonDataFilePath;
 
     @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public JsonData getJsonData() {
-        return new JsonData();
+        return new JsonData(this.jsonDataFilePath);
     }
 }
--- src/main/java/com/example/demo/data/JsonData.java
+++ src/main/java/com/example/demo/data/JsonData.java
@@ -1,4 +1,9 @@
 package com.example.demo.data;
 
 public class JsonData {
+    private final String jsonDataFilePath;
+
+    public JsonData(String jsonDataFilePath) {
+        this.jsonDataFilePath = jsonDataFilePath;
+    }
 }
