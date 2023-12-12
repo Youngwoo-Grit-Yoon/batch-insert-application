6773113a44e519c939dda60686524ecbf3afd316
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-12 13:28:58 +0900

    Add VDN entity class

---
+++ src/main/java/com/example/demo/entity/VDN.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="tb_bm_mn_vdn")
+public class VDN {
+
+}
