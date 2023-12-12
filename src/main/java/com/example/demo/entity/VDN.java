5a7c98b6a379df228cb28cee56a9e1b77bcdd27f
윤영우 <youngwooyoon@yoon-young-woo-MacBookPro.local>
2023-12-12 14:48:39 +0900

    Add VDN class

--- src/main/java/com/example/demo/entity/VDN.java
+++ src/main/java/com/example/demo/entity/VDN.java
@@ -9,7 +9,34 @@ import lombok.Data;
 public class VDN {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String vdn_sq_id;
-    private String vdn_no;
-    private String center_id;
+    @Column(name = "vdn_sq_id")
+    private String vdnSqId;
+    @Column(name = "vdn_no")
+    private String vdnNo;
+    @Column(name = "center_id")
+    private String centerId;
+    @Column(name = "server_id")
+    private String serverId;
+    @Column(name = "monitoring_yn")
+    private String monitoringYn;
+    @Column(name = "vdn_type")
+    private String vdnType;
+    @Column(name = "split")
+    private String split;
+    @Column(name = "check_link")
+    private String checkLink;
+    @Column(name = "comment")
+    private String comment;
+    @Column(name = "result")
+    private String result;
+    @Column(name = "regi_user_id")
+    private String regiUserId;
+    @Column(name = "regi_dttm")
+    private String regiDttm;
+    @Column(name = "updt_user_id")
+    private String updtUserId;
+    @Column(name = "updt_dttm")
+    private String updtDttm;
+    @Column(name = "modify_flag")
+    private String modifyFlag;
 }
